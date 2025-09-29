4cd62e8706179254dc873c203fca3ccee1193db0
Denis Hideki Yoshida <denhiyo82@gmail.com>
2025-09-29 18:23:58 -0300

    feat: ajuste na classe security para permitir abrir o swagger no browser

--- src/main/java/br/com/ntt/bank/configs/SecurityConfig.java
+++ src/main/java/br/com/ntt/bank/configs/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
